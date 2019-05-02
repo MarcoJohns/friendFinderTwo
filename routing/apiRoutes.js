a1abb02de1d364c0dd950579b4f39229deacd2dd
marco <marcobjohns@gmail.com>
2019-05-02 12:25:11 -0600

    Set up my file system and included node_modulesin my .gitignore file.

---
+++ routing/apiRoutes.js
@@ -0,0 +1,19 @@
+app.get("/api/frinds", function (req, res) {
+    return res.json(characters);
+});
+
+app.post("/api/friends", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newcharacter = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newcharacter);
+
+    characters.push(newcharacter);
+
+    res.json(newcharacter);
+});
---
+++ routing/htmlRoutes.js
@@ -0,0 +1,8 @@
+app.get("/", function (req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "./../public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "./../public/survey.html"));
+});
